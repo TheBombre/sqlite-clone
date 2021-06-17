62306b4bea189e160fd45d97834a04f998cf8d23
TheBombre <carlton42@mailbox.org>
2021-06-17 17:52:49 +0200

    Begin work on database

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,14 @@
+package app;
+
+public class Main
+{
+    public String getGreeting()
+    {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(new Main().getGreeting());
+    }
+}
